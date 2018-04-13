ff4256ceec1325be51beda6436f7bb9c2b7012b6
Trina <tcjcastillo@gmail.com>
2018-04-13 16:32:37 -0700

    react router working + posts fetched

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { applyMiddleware, createStore } from 'redux';
+import PostsIndex from './components/PostsIndex';
+import PostsNew from './components/PostsNew';
 import reducers from './reducers';
+import reduxPromise from 'redux-promise';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(reduxPromise)(createStore);
+
+class Hello extends React.Component {
+  render() {
+    return <div>Hello!</div>;
+  }
+}
+class Goodbye extends React.Component {
+  render() {
+    return <div>Goodbye :(</div>;
+  }
+}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector('.container')
+);
