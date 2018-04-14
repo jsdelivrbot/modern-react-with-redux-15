2527cf3dd25162095ba2d7e70297a4df6f8da1bf
Trina <tcjcastillo@gmail.com>
2018-04-13 19:30:13 -0700

    validation + highlighting

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -6,25 +6,64 @@ class PostsNew extends Component {
     super(props);
   }
 
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
     return (
-      <form>
-        <Field label='Title for Post' name="title" component={this.renderField} />
-        <Field label='Tags' name="tags" component={this.renderField} />
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label="Title for Post"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
       </form>
     );
   }
 
   renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error && 'has-danger'}`;
     return (
-      <div className="form-group">
+      <div className={className}>
         <label htmlFor="">{field.label}</label>
         <input className="form-control" type="text" {...field.input} />
+        {touched && <p className='text-help'>{error}</p>}
       </div>
     );
   }
 }
 
+const validate = (values) => {
+  const errors = {};
+  if (!values.title) {
+    errors.title = 'Enter a title';
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter a categories';
+  }
+  if (!values.content) {
+    errors.content = 'Enter a content';
+  }
+  return errors;
+};
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm',
 })(PostsNew);
